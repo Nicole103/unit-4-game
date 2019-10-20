ee3a62b24f9bedd8a24e166f6258112cef499154
Nicole Heersink <nheersink8@gmail.com>
2019-10-20 16:25:31 -0600

    adding the pseudo js code

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// the Crystal Collector Game JS //
+
+
+//------varables------//
+
+//varables for counting the wins and losses//
+
+//varables for each crystal random number for turn//
+    //red, blue, yellow, green//
+
+//varable for matching number //
+    //computerChoice, computerOptions//
+
+//varable for current count/sum of numbers//
+
+
+//------functions-------//
+
+//if function to continue until either the player over clicks or under clicks (if the sum is < the match number click again)//
+//else if function - if the sum is === the match number mark wins and reset function//
+//else function- if sum is > the match number mark looses and reset function//
+
+//reset function//
+    //reset the varables for each crystal to new random number//
+    //computer choose a new match number//
+    //reset sum counter to 0//
+
+//randomization for each crystal for the turn//
+//randomly select a number the user will try to match by clicking on the crystals//
+    //computerchoice function//
+//restart/reset function for the user to try again. this will reset the selected random numbers for all 5 fields//
+
