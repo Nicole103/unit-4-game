cd3912c27b34055772685cbebf3d21069fdd895c
Nicole Heersink <nheersink8@gmail.com>
2019-10-22 21:53:07 -0600

    updating assets folder

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,32 @@
 //------varables------//
 
 //varables for counting the wins and losses//
+var wins = 0;
+var looses = 0;
 
 //varables for each crystal random number for turn//
     //red, blue, yellow, green//
+var redCrystal = " ";
+var blueCrystal = " ";
+var yellowCrystal = " ";
+var greenCrystal = " ";
 
 //varable for matching number //
     //computerChoice, computerOptions//
+var selectedMatchNumber = " ";
 
 //varable for current count/sum of numbers//
+var currentUserSum = 0;
+var counter = 0;
 
 
 //------functions-------//
+//onclick function to take the value from the image to add to the sum//
+$("#red-crystal").on("click", function() {
+
+    alert("you clicked on the red crystal!");
+
+});
 
 //if function to continue until either the player over clicks or under clicks (if the sum is < the match number click again)//
 //else if function - if the sum is === the match number mark wins and reset function//
