655a72b744176aa808e74eb904c8702bc971ca97
Nicole Heersink <nheersink8@gmail.com>
2019-10-26 21:00:44 -0600

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,10 @@ var losses = 0;
 
 //varables for each crystal random number for turn//
     //red, blue, yellow, green//
-var redCrystalOptions = ["1","2","3","4"];
-var blueCrystalOptions = ["5","7","9","11"];
-var purpleCrystalOptions = ["8","12","16","20"];
-var greenCrystalOptions = ["25","30","35","40"];
+var redCrystalOptions = ["1","2","3"];
+var blueCrystalOptions = ["4","5","6"];
+var purpleCrystalOptions = ["7","8","9"];
+var greenCrystalOptions = ["10","11","12"];
 
 var redCrystal = " ";
 var blueCrystal = " ";
@@ -20,7 +20,15 @@ var purpleCrystal = " ";
 var greenCrystal = " ";
 //varable for matching number //
     //computerChoice, computerOptions//
-var matchNumberOptions = ["75","77","84","81","99","100","101","113","120","127"]
+var matchNumberOptions = [ "19", "20", "21", "22", "23", "24"," 25", "26",
+    "27", "28", "29", "30", "31", "32", "33", "34","35", "36", "37", "38",
+    "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+    "51", "52", "53"," 54", "55", "56", "57", "58", "59", "60", "61", "62",
+    "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", 
+    "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", 
+    "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98",
+    "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109",
+    "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120"]
 var selectedMatchNumber = " ";
 
 
